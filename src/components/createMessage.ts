a3a97386a4a68a0d35a97b0d2b754ebab655a539
laixin <774612160@qq.com>
2020-10-27 18:25:07 +0800

    commit

--- src/components/createMessage.ts
+++ src/components/createMessage.ts
@@ -14,7 +14,7 @@ const createMessage = (message: string, type: MessageType, timeout = 2000) => {
   const mountNode = document.createElement('div') // 创建一个节点给Message组件挂载
   document.body.appendChild(mountNode) // 在body上插入
   messageInstance.mount(mountNode) // 调用mount方法, 把Message组件挂载在刚刚创建的节点上
-  const timer = setTimeout(() => {
+  setTimeout(() => {
     messageInstance.unmount(mountNode)
     document.body.removeChild(mountNode)
   }, timeout)
